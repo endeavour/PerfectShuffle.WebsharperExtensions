7a3d2cc5d20dc23e09a3353bab233ee4054d75d3
James Freiwirth <james@perfectshuffle.co.uk>
2013-02-15 03:01:18 +0000

    Initial commit

---
+++ WebsharperExtensions/Extensions.fs
@@ -0,0 +1,23 @@
+﻿namespace PerfectShuffle.WebsharperExtensions
+open IntelliFactory.WebSharper.Sitelets
+
+module Context =
+  let map (context:Context<'a>) (inverseActionMap:'b -> 'a) : Context<'b> =
+    let newContext:Context<'b> =
+      {
+        ApplicationPath = context.ApplicationPath
+        Link = inverseActionMap >> context.Link
+        Json = context.Json
+        Metadata = context.Metadata
+        ResolveUrl = context.ResolveUrl
+        ResourceContext = context.ResourceContext
+        Request = context.Request
+      }
+    newContext
+
+module Content =
+   let map (actionMap:'a -> 'b) (content:Content<'a>) : Content<'b> =
+     match content with
+     | CustomContent f -> CustomContent (fun context -> f (Context.map context actionMap))
+     | PageContent f -> PageContent (fun context -> f (Context.map context actionMap))
+
