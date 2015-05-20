dede4e654f0051609e20e09bc95e6cdf82d36665
James Freiwirth <james@perfectshuffle.co.uk>
2015-05-20 18:43:26 +0100

    Add UI.Next extensions

---
+++ PerfectShuffle.WebsharperExtensions/UINext.fs
@@ -0,0 +1,10 @@
+﻿namespace PerfectShuffle.WebsharperExtensions.UI
+
+module Next =
+  open WebSharper.UI.Next
+  
+  type View with
+      static member Sequence views =
+          views
+          |> Seq.fold (View.Map2 (fun a b -> 
+              seq { yield! a; yield b })) (View.Const Seq.empty)
