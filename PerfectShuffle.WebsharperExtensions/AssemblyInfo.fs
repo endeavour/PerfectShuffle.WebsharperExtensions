d3fe7e60634ce5e50b89e80702c1958cab0cccd4
James Freiwirth <jamesfreiwirth@digiterre.com>
2016-05-09 17:26:27 +0100

    Started work porting to paket

--- PerfectShuffle.WebsharperExtensions/AssemblyInfo.fs
+++ PerfectShuffle.WebsharperExtensions/AssemblyInfo.fs
@@ -1,14 +1,41 @@
-﻿namespace System
+﻿namespace PerfectShuffle.EventSourcing.AssemblyInfo
+
 open System.Reflection
+open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [<assembly: AssemblyTitleAttribute("PerfectShuffle.WebsharperExtensions")>]
 [<assembly: AssemblyDescriptionAttribute("Various odds and ends for Websharper projects")>]
-[<assembly: GuidAttribute("34e4036c-e16c-4cc4-84d3-820207ec5837")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProductAttribute("PerfectShuffle.WebsharperExtensions")>]
-[<assembly: AssemblyVersionAttribute("0.1.19")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.19")>]
-do ()
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
 
-module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.19"
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: GuidAttribute("34e4036c-e16c-4cc4-84d3-820207ec5837")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//       Major Version
+//       Minor Version
+//       Build Number
+//       Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("0.2.*")>]
+[<assembly: AssemblyFileVersion("0.2.*")>]
+
+do
+    ()
